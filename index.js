4b4f9bff7d8575d5940211d6a92a3f4aab7c8bc6
Duncan <njorogedun870@gmail.com>
2024-07-18 08:52:22 +0300

    pushed all changes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+window.customerName = 'bob';
+
+const leastFavoriteCustomer = 'someone';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'new least favorite';
+  } catch (e) {
+    throw new Error('Assignment to constant variable.');
+  }
+}
+
